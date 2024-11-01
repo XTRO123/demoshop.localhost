6cee477212f74f7698caadbe124cf6250a6c45f1
Andrey <0test@mail.ru>
2024-11-01 18:44:49 +0300

    params

--- views/pages/product.blade.php
+++ views/pages/product.blade.php
@@ -8,8 +8,9 @@
         <article class="row mt-5" itemscope itemtype="https://schema.org/Product">
             <div class="col-lg-4 col-md-6 col-12">
                 <figure>
-                    <img id="main_image" data-src="@phpthumb($product_photo,'w=500')" src="@phpthumb($product_photo,'w=500')"
-                        loading="lazy" class="img-thumbnail" alt="{{$pagetitle}}" itemprop="image">
+                    <img id="main_image" data-src="@phpthumb($product_photo,'w=500')"
+                        src="@phpthumb($product_photo,'w=500')" loading="lazy" class="img-thumbnail"
+                        alt="{{$pagetitle}}" itemprop="image">
                 </figure>
             </div>
             <div class="col-lg-8 col-md-6 col-12">
@@ -20,10 +21,32 @@
                     Price: <span id="price" itemprop="price" content="{{ $price }}">@price($price)</span>
                     <meta itemprop="priceCurrency" content="RUB" />
                 </div>
-                
+
+                <div class="mt-3">
+                    <div class=" ">
+                        <div class="align-content-center d-flex">
+                            <div class="me-1 p-1">Brand:</div>
+                            <div class="bg-body-tertiary border p-1 rounded-2">{{$brand_value}}</div>
+                        </div>
+                    </div>
+                    <div class=" mt-2">
+                        <div class="align-content-center d-flex">
+                            <div class="me-1 p-1">Color:</div>
+                            <div class="bg-body-tertiary border p-1 rounded-2">{{$color_value}}</div>
+                        </div>
+                    </div>
+                </div>
+
                 <div class="mt-3">
                     <form data-commerce-action="add" data-instance="{{ $cart_instance }}" itemprop="offers" itemscope
                         itemtype="https://schema.org/Offer">
+
+                        <!-- options -->
+                        <input type="hidden" name="options[product_brand]" value="{{$brand_value}}">
+                        <input type="hidden" name="options[product_color]" value="{{$color_value}}">
+                        <!-- /END options -->
+
+
                         <input type="hidden" name="id" value="{{$id}}">
                         <input type="hidden" name="count" value="1">
 
@@ -43,6 +66,22 @@
                 </div>
             </div>
         </article>
+    </div>
+</div>
+
+<div class="container my-5">
+    <div class="col-lg-12 col-12 px-0">
+        <h2>Relative:</h2>
+        @if ($relative)
+        <section class="row mt-5">
+            @foreach ($relative as $product)
+            @include('parts.product_in_feed')
+            @endforeach
+        </section>
+        <div class="row">
+            {!! $pages ?? '' !!}
+        </div>
+        @endif
     </div>
 </div>
 @endsection
